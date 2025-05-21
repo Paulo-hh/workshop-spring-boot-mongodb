4744243400a25f8de0505f19df0ad756fb260f90
PauloHoff <paulohhoff@gmail.com>
2025-05-21 18:57:21 -0300

    Insert user

--- src/main/java/com/hoffpaulo/workshopmongo/services/UserService.java
+++ src/main/java/com/hoffpaulo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hoffpaulo.workshopmongo.domain.User;
+import com.hoffpaulo.workshopmongo.dto.UserDTO;
 import com.hoffpaulo.workshopmongo.repositories.UserRepository;
 import com.hoffpaulo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -28,5 +29,13 @@ public class UserService {
 		catch(NoSuchElementException e) {
 			throw new ObjectNotFoundException("Object not found");
 		}
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
