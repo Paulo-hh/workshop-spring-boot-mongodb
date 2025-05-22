7e513f1b3900519d854eba17ed1073c2426bb8c2
PauloHoff <paulohhoff@gmail.com>
2025-05-22 01:10:03 -0300

    AuthorDTO created

--- src/main/java/com/hoffpaulo/workshopmongo/domain/Post.java
+++ src/main/java/com/hoffpaulo/workshopmongo/domain/Post.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hoffpaulo.workshopmongo.dto.AuthorDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,12 +20,12 @@ public class Post implements Serializable{
 	private LocalDate date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 
 	public Post() {
 	}
 
-	public Post(String id, LocalDate date, String title, String body, User author) {
+	public Post(String id, LocalDate date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -64,11 +65,11 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
