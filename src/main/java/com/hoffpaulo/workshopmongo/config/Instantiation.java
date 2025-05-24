3915d8d6c1d49be658b8d1149e0bd24a26d9dc46
PauloHoff <paulohhoff@gmail.com>
2025-05-23 22:55:28 -0300

    Add comments to posts

--- src/main/java/com/hoffpaulo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/hoffpaulo/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.hoffpaulo.workshopmongo.domain.Post;
 import com.hoffpaulo.workshopmongo.domain.User;
 import com.hoffpaulo.workshopmongo.dto.AuthorDTO;
+import com.hoffpaulo.workshopmongo.dto.CommentDTO;
 import com.hoffpaulo.workshopmongo.repositories.PostRepository;
 import com.hoffpaulo.workshopmongo.repositories.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, LocalDate.parse("21/03/2018", dtf), "Partiu viagem!", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, LocalDate.parse("23/03/2018", dtf), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa Viagem mano!", LocalDate.parse("21/03/2018", dtf), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", LocalDate.parse("22/03/2018", dtf), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", LocalDate.parse("23/03/2018", dtf), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
---
+++ src/main/java/com/hoffpaulo/workshopmongo/dto/CommentDTO.java
@@ -0,0 +1,47 @@
+package com.hoffpaulo.workshopmongo.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class CommentDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String text;
+	private LocalDate data;
+	private AuthorDTO author;
+	
+	public CommentDTO() {
+	}
+
+	public CommentDTO(String text, LocalDate data, AuthorDTO author) {
+		this.text = text;
+		this.data = data;
+		this.author = author;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+	
+}
